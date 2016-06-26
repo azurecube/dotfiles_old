6ed9fa1bfe346d1c456faddfde015e434de2d0dc
azurecube <bw@azurecube.org>
2016-06-26 12:07:13 +0900

    prevent from creating nested symbolic links

--- link.sh
+++ link.sh
@@ -2,5 +2,8 @@
 
 DOT_ROOT=$(cd $(dirname $0);pwd)
 for i in $(ls $DOT_ROOT/dot);do
-  ln -s $DOT_ROOT/dot/$i ~/'.'$i
+  source=$DOT_ROOT/dot/$i
+  dest=~/'.'$i
+  if [ -e $dest ]; then continue; fi
+  ln -s  $source $dest && echo link $dest is created
 done
