f1b1a986798d7d395006690af3e401c415b4cde8
azurecube <bw@azurecube.org>
2016-09-11 16:56:59 -0700

    add zplug

---
+++ dot/zsh.d/zplug.sh
@@ -0,0 +1,19 @@
+source ~/.zplug/init.zsh
+
+zplug 'zsh-users/zsh-autosuggestions'
+zplug 'zsh-users/zsh-completions'
+zplug 'zsh-users/zsh-syntax-highlighting'
+zplug 'mollifier/anyframe'
+zplug "b4b4r07/enhancd", use:init.sh
+zplug "peco/peco", as:command, from:gh-r, use:"*amd64*"
+zplug "b4b4r07/dotfiles", as:command, use:bin/peco-tmux
+
+if ! zplug check --verbose; then
+  printf 'Install? [y/N]: '
+  if read -q; then
+    echo; zplug install
+  fi
+fi
+
+zplug load --verbose
+
