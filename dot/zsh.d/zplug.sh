2f4895f5a08ead68a769dabf9bc29c060de1cc20
azurecube <bw@azurecube.org>
2016-09-11 17:29:00 -0700

    add ghq

--- dot/zsh.d/zplug.sh
+++ dot/zsh.d/zplug.sh
@@ -7,6 +7,8 @@ zplug 'mollifier/anyframe'
 zplug "b4b4r07/enhancd", use:init.sh
 zplug "peco/peco", as:command, from:gh-r, use:"*amd64*"
 zplug "b4b4r07/dotfiles", as:command, use:bin/peco-tmux
+#zplug "motemen/ghq", as:command, from:gh-r, rename-to:ghq
+zplug "motemen/ghq", as:command, from:gh-r, use:bin/:ghq
 
 if ! zplug check --verbose; then
   printf 'Install? [y/N]: '
