7abf30b96f5604a4ddd27a2e469f305e59345c79
azurecube <bw@azurecube.org>
2016-09-11 18:30:52 -0700

    modify ghq name

--- dot/zsh.d/zplug.sh
+++ dot/zsh.d/zplug.sh
@@ -7,8 +7,12 @@ zplug 'mollifier/anyframe'
 zplug "b4b4r07/enhancd", use:init.sh
 zplug "peco/peco", as:command, from:gh-r, use:"*amd64*"
 zplug "b4b4r07/dotfiles", as:command, use:bin/peco-tmux
-#zplug "motemen/ghq", as:command, from:gh-r, rename-to:ghq
-zplug "motemen/ghq", as:command, from:gh-r, use:bin/:ghq
+zplug "motemen/ghq", as:command, from:gh-r, use:bin/ghq
+#zplug "arks22/tmuximum", as:command
+#zplug "junegunn/fzf", as:command, use:bin/fzf-tmux
+#zplug "junegunn/fzf-bin", from:gh-r, as:command, rename-to:fzf, use:"*amd64*"
+
+
 
 if ! zplug check --verbose; then
   printf 'Install? [y/N]: '
