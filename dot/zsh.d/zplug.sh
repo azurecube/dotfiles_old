d7379bd8ecd41e694c25e0e78fa532e60679bcf0
azurecube <bw@azurecube.org>
2016-09-23 17:25:51 -0700

    remove completions from zplug

--- dot/zsh.d/zplug.sh
+++ dot/zsh.d/zplug.sh
@@ -9,8 +9,8 @@ zplug "b4b4r07/enhancd", use:init.sh
 zplug "b4b4r07/dotfiles", as:command, use:bin/peco-tmux
 #zplug "motemen/ghq", as:command, from:gh-r, use:bin/ghq
 #zplug "arks22/tmuximum", as:command
-#zplug "junegunn/fzf", as:command, use:bin/fzf-tmux
 #zplug "junegunn/fzf-bin", from:gh-r, as:command, rename-to:fzf, use:"*amd64*"
+#zplug "junegunn/fzf", as:command, use:bin/fzf-tmux
 
 
 
