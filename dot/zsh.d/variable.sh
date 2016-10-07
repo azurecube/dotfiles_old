8bcd97851d8664c257aa7ae8267b1e185a54ffbb
azurecube <bw@azurecube.org>
2016-10-07 13:03:58 +0900

    arrange PATH order

--- dot/zsh.d/variable.sh
+++ dot/zsh.d/variable.sh
@@ -2,8 +2,8 @@
 export CDPATH=.:~/
 export DIRSTACK=50
 export GOPATH=~/
-export PATH=$PATH:$GOPATH:~/bin
 export EDITOR=/usr/bin/vi
 export ENHANCD_COMMAND=ecd
 export LAB_ROOT=~/lib/lab
 export BROWSER=firefox
+export PATH=$PATH:$GOPATH:~/bin:$LAB_ROOT/bin:$LAB_ROOT/nodes
