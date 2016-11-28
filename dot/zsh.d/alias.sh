51c99a741c2234d972b82185584604fe633202e0
azurecube <bw@azurecube.org>
2016-11-28 09:44:06 +0900

    add xclip operation

--- dot/zsh.d/alias.sh
+++ dot/zsh.d/alias.sh
@@ -4,6 +4,8 @@ alias asearch="sudo apt-cache search"
 alias adep="sudo apt-get build-dep"
 alias taskshell='ZDOTDIR=~/.task zsh'
 alias emacs='emacs -nw'
+alias xclip='xclip -selection c'
+alias p2c='xclip -o -selection p | xclip -selection c'
 
 #Directory
 #alias j="autojump"
