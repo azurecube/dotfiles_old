8ac3cc3f531a42e0030e7096dcb03bb1c7fdd201
azurecube <bw@azurecube.org>
2017-01-04 21:51:40 +0900

    add sfdc

--- dot/zsh.d/variable.sh
+++ dot/zsh.d/variable.sh
@@ -8,4 +8,4 @@ export LAB_ROOT=~/lib/lab
 export BROWSER=firefox
 export JAVA_HOME=/usr/java/jdk
 export ZPLUG_HOME=~/lib/zplug
-export PATH=$PATH:$GOPATH:~/bin:$LAB_ROOT/bin:$LAB_ROOT/nodes:$JAVA_HOME/bin:~/lib/tmacs/bin
+export PATH=$PATH:$GOPATH:~/bin:$LAB_ROOT/bin:$LAB_ROOT/nodes:$JAVA_HOME/bin:~/lib/tmacs/bin:~/lib/sfdc/bin
